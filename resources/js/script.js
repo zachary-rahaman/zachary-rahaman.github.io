c129b60acc6b0789b29b3552337b2be2f1fd1cf4
zachary-rahaman <rahamanzachary3@gmail.com>
2025-04-01 19:35:14 -0400

    JS Edits

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,4 @@
-function enterSite() {
-    document.getElementById('welcome').classList.add('hidden');
-    document.getElementById('about').classList.remove('hidden');
-}
-
-  function showTab(tabId) {
+function showTab(tabId) {
     const tabs = document.querySelectorAll('.tab-content');
     tabs.forEach(tab => tab.classList.remove('active'));
     document.getElementById(tabId).classList.add('active');
