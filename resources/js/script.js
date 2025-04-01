78afe7b99958bcaaec97d3bf80c3700ce707f89f
zachary-rahaman <rahamanzachary3@gmail.com>
2025-04-01 19:21:05 -0400

    Single Page Format Transformation

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,3 +8,11 @@ function enterSite() {
     tabs.forEach(tab => tab.classList.remove('active'));
     document.getElementById(tabId).classList.add('active');
 }
+
+function enterSite() {
+    document.getElementById('welcome').classList.add('hidden');
+    document.getElementById('about').classList.remove('hidden');
+    const title = document.getElementById('site-title');
+    title.classList.remove('centered-title');
+    title.classList.add('top-title');
+}
